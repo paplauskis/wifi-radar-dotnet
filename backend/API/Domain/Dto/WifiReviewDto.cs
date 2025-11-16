f725601b8b2bcba3d22585969153506aee611dad
paplauskis <paplauskaskostas@gmail.com>
2025-11-16 12:37:12 +0200

    add WifiId property

--- backend/API/Domain/Dto/WifiReviewDto.cs
+++ backend/API/Domain/Dto/WifiReviewDto.cs
@@ -1,11 +1,12 @@
-namespace API.Domain;
+namespace API.Domain.Dto;
 
 public class WifiReviewDto
 {
+    public string? WifiId { get; set; }
     public string? City { get; set; }
     public string? Street { get; set; }
     public int? BuildingNumber { get; set; }
-    public string UserId { get; set; }
+    public string? UserId { get; set; }
     public int? Rating { get; set; }
     public string? Text { get; set; }
 }
