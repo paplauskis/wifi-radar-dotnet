1b0f092796bd9e9269885c1e694f98b80a68acef
paplauskis <paplauskaskostas@gmail.com>
2025-11-16 12:37:32 +0200

    add UserId property

--- backend/API/Domain/Dto/PasswordDto.cs
+++ backend/API/Domain/Dto/PasswordDto.cs
@@ -1,7 +1,8 @@
-namespace API.Domain;
+namespace API.Domain.Dto;
 
 public class PasswordDto
 {
+    public string? UserId { get; set; }
     public string? City { get; set; }
     public string? Street { get; set; }
     public int? BuildingNumber { get; set; }
