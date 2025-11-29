dba6c080fddc8ad2f5c39188feb5625dd6e1a7a7
paplauskis <paplauskaskostas@gmail.com>
2025-11-29 13:35:15 +0200

    add address to dtos, remove wifi id

--- backend/Tests/Helpers/WifiReviewDtoHelper.cs
+++ backend/Tests/Helpers/WifiReviewDtoHelper.cs
@@ -13,50 +13,60 @@ public static class WifiReviewDtoHelper
             {
                 new WifiReviewDto
                 {
-                    WifiId = "randomID12313",
                     UserId = "u1wfv4jn1w2uv982349",
-                    Text = "Good connection, does not lag, NICE",
-                    Rating = 9
+                    Text = "    ",
+                    Rating = 9,
+                    City = "Vilnius",
+                    Street = "Gedimino pr.",
+                    BuildingNumber = 10
                 }
             },
             new object[]
             {
                 new WifiReviewDto
                 {
-                    WifiId = "i",
                     UserId = "user9876",
                     Text = "Very poor signal, disconnects often.",
-                    Rating = 3
+                    Rating = 3,
+                    City = "Kaunas",
+                    Street = "Savanorių pr.",
+                    BuildingNumber = 22
                 }
             },
             new object[]
             {
                 new WifiReviewDto
                 {
-                    WifiId = ObjectId.GenerateNewId().ToString(),
-                    UserId = "rthb",
+                    UserId = ObjectId.GenerateNewId().ToString(),
                     Text = "Okayish speed, usable for browsing.",
-                    Rating = 6
+                    Rating = 0,
+                    City = "Vilnius",
+                    Street = "Ozo g.",
+                    BuildingNumber = 18
                 }
             },
             new object[]
             {
                 new WifiReviewDto
                 {
-                    WifiId = ObjectId.GenerateNewId().ToString(),
                     UserId = ObjectId.GenerateNewId().ToString(),
                     Text = new string('a', 122),
-                    Rating = 7
+                    Rating = 7,
+                    City = "Klaipėda",
+                    Street = "H. Manto g.",
+                    BuildingNumber = 7
                 }
             },
             new object[]
             {
                 new WifiReviewDto
                 {
-                    WifiId = ObjectId.GenerateNewId().ToString(),
                     UserId = ObjectId.GenerateNewId().ToString(),
                     Text = "Terrible experience!",
-                    Rating = 11
+                    Rating = 11,
+                    City = "Vilnius",
+                    Street = "Pylimo g.",
+                    BuildingNumber = 21
                 }
             }
         };
@@ -70,50 +80,60 @@ public static class WifiReviewDtoHelper
             {
                 new WifiReviewDto
                 {
-                    WifiId = ObjectId.GenerateNewId().ToString(),
                     UserId = ObjectId.GenerateNewId().ToString(),
                     Text = "Good connection, does not lag, NICE",
-                    Rating = 9
+                    Rating = 9,
+                    City = "Vilnius",
+                    Street = "Jasinskio g.",
+                    BuildingNumber = 56
                 }
             },
             new object[]
             {
                 new WifiReviewDto
                 {
-                    WifiId = ObjectId.GenerateNewId().ToString(),
                     UserId = ObjectId.GenerateNewId().ToString(),
                     Text = "Very poor signal, disconnects often.",
-                    Rating = 3
+                    Rating = 3,
+                    City = "Kaunas",
+                    Street = "Laisvės al.",
+                    BuildingNumber = 45
                 }
             },
             new object[]
             {
                 new WifiReviewDto
                 {
-                    WifiId = ObjectId.GenerateNewId().ToString(),
                     UserId = ObjectId.GenerateNewId().ToString(),
                     Text = "Okayish speed, usable for browsing.",
-                    Rating = 6
+                    Rating = 6,
+                    City = "Klaipėda",
+                    Street = "Turgaus g.",
+                    BuildingNumber = 12
                 }
             },
             new object[]
             {
                 new WifiReviewDto
                 {
-                    WifiId = ObjectId.GenerateNewId().ToString(),
                     UserId = ObjectId.GenerateNewId().ToString(),
                     Text = new string("good enough"),
-                    Rating = 7
+                    Rating = 7,
+                    City = "Vilnius",
+                    Street = "Ozo g.",
+                    BuildingNumber = 18
                 }
             },
             new object[]
             {
                 new WifiReviewDto
                 {
-                    WifiId = ObjectId.GenerateNewId().ToString(),
                     UserId = ObjectId.GenerateNewId().ToString(),
                     Text = "Terrible experience!",
-                    Rating = 2
+                    Rating = 2,
+                    City = "Kaunas",
+                    Street = "Savanorių pr.",
+                    BuildingNumber = 22
                 }
             }
         };
