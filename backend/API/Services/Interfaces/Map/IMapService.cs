483b2a334effa86eac2884bb4f92120cd1605d52
paplauskis <paplauskaskostas@gmail.com>
2025-12-14 18:01:03 +0200

    add SearchAndSaveToFile method

--- backend/API/Services/Interfaces/Map/IMapService.cs
+++ backend/API/Services/Interfaces/Map/IMapService.cs
@@ -1,3 +1,4 @@
+using API.Domain.Dto;
 using API.Domain.Dto.OverpassDto;
 
 namespace API.Services.Interfaces.Map;
@@ -5,6 +6,7 @@ namespace API.Services.Interfaces.Map;
 public interface IMapService
 { 
     Task<List<OverpassResponseElementDto>> Search(string city, int? radius);
+    Task<FileResponseDto> SearchAndSaveToFile(string city, string? fileType, int? radius);
     Task<List<OverpassResponseElementDto>> SearchWifisFromNearestCity(string ip);
     Task<CoordinateDto> GetCoordinates(string city, string street, int buildingNumber);
 }
